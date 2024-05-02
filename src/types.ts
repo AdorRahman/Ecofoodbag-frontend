3ec798ebd9deb059c437e53a18edc226e1770c29
AdorRahman <ador1234ador@gmail.com>
2024-05-03 03:57:26 +0600

    update

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,7 @@ export type User = {
 	city: string;
 	country: string;
 };
+
 export type MenuItem = {
 	_id: string;
 	name: string;
@@ -24,6 +25,34 @@ export type Restaurant = {
 	menuItems: MenuItem[];
 	imageUrl: string;
 	lastUpdated: string;
+};
+
+export type OrderStatus =
+	| "placed"
+	| "paid"
+	| "inProgress"
+	| "outForDelivery"
+	| "delivered";
+
+export type Order = {
+	_id: string;
+	restaurant: Restaurant;
+	user: User;
+	cartItems: {
+		menuItemId: string;
+		name: string;
+		quantity: string;
+	}[];
+	deliveryDetails: {
+		name: string;
+		addressLine1: string;
+		city: string;
+		email: string;
+	};
+	totalAmount: number;
+	status: OrderStatus;
+	createdAt: string;
+	restaurantId: string;
 };
 
 export type RestaurantSearchResponse = {
