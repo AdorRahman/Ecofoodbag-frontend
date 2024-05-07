3aa4944a1de657a0b8a699c08918d8e0573b8706
AdorRahman <ador1234ador@gmail.com>
2024-05-07 22:45:18 +0600

    update

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useGetMyUser } from "@/api/MyUserApi";
 
 const MobileNavLinks = () => {
 	const { logout } = useAuth0();
+	const { currentUser, isLoading: isGetUserLoading } = useGetMyUser();
+
+	console.log(currentUser);
+
+	if (isGetUserLoading) {
+		return <></>;
+	}
 	return (
 		<>
-			<Link
-				to='/order-status'
-				className='flex items-center font-bold bg-white hover:text-green-500'>
-				Order Status
-			</Link>
-			<Link
-				to='/manage-restaurant'
-				className='flex items-center font-bold bg-white hover:text-green-500'>
-				My Restaurant
-			</Link>
+			{currentUser?.isVendor ? (
+				<Link
+					to='/manage-restaurant'
+					className='font-bold hover:text-green-500'>
+					Manage Restaurant
+				</Link>
+			) : (
+				<Link to='/become-vendor' className='font-bold hover:text-green-500'>
+					Become a Vendor
+				</Link>
+			)}
 			<Link
 				to='/user-profile'
 				className='flex items-center font-bold bg-white hover:text-green-500'>
 				User Profile
+			</Link>
+			<Link
+				to='/order-status'
+				className='flex items-center font-bold bg-white hover:text-green-500'>
+				Order Status
 			</Link>
 			<Button
 				onClick={() => logout()}
