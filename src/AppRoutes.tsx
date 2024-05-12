46765793217bf839f4cb578a08e0c0c21934cdfb
AdorRahman <ador1234ador@gmail.com>
2024-05-13 00:39:06 +0600

    update

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -13,87 +13,87 @@ import TermsAndConditions from "./pages/TermsAndConditions";
 import BecomeVendor from "./pages/BecomeVendorPage";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout showHero>
-            <HomePage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/Privacy-Policy"
-        element={
-          <Layout>
-            <PrivacyPolicy />
-          </Layout>
-        }
-      />
-      <Route
-        path="/Terms-and-Conditions"
-        element={
-          <Layout>
-            <TermsAndConditions />
-          </Layout>
-        }
-      />
-      <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route
-        path="/search/:city"
-        element={
-          <Layout showHero={false}>
-            <SearchPage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/detail/:restaurantId"
-        element={
-          <Layout showHero={false}>
-            <DetailPage />
-          </Layout>
-        }
-      />
-      <Route element={<ProtectedRoute />}>
-        <Route
-          path="/order-status"
-          element={
-            <Layout>
-              <OrderStatusPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/user-profile"
-          element={
-            <Layout>
-              <UserProfilePage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/manage-restaurant"
-          element={
-            <Layout>
-              <ManageRestaurantPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/become-vendor"
-          element={
-            <Layout>
-              <BecomeVendor />
-            </Layout>
-          }
-        />
-      </Route>
+	return (
+		<Routes>
+			<Route
+				path='/'
+				element={
+					<Layout showHero>
+						<HomePage />
+					</Layout>
+				}
+			/>
+			<Route
+				path='/privacy-policy'
+				element={
+					<Layout>
+						<PrivacyPolicy />
+					</Layout>
+				}
+			/>
+			<Route
+				path='/terms-and-conditions'
+				element={
+					<Layout>
+						<TermsAndConditions />
+					</Layout>
+				}
+			/>
+			<Route path='/auth-callback' element={<AuthCallbackPage />} />
+			<Route
+				path='/search/:city'
+				element={
+					<Layout showHero={false}>
+						<SearchPage />
+					</Layout>
+				}
+			/>
+			<Route
+				path='/detail/:restaurantId'
+				element={
+					<Layout showHero={false}>
+						<DetailPage />
+					</Layout>
+				}
+			/>
+			<Route element={<ProtectedRoute />}>
+				<Route
+					path='/order-status'
+					element={
+						<Layout>
+							<OrderStatusPage />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/user-profile'
+					element={
+						<Layout>
+							<UserProfilePage />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/manage-restaurant'
+					element={
+						<Layout>
+							<ManageRestaurantPage />
+						</Layout>
+					}
+				/>
+				<Route
+					path='/become-vendor'
+					element={
+						<Layout>
+							<BecomeVendor />
+						</Layout>
+					}
+				/>
+			</Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+			<Route path='*' element={<Navigate to='/' />} />
+		</Routes>
+	);
 };
 
 export default AppRoutes;
